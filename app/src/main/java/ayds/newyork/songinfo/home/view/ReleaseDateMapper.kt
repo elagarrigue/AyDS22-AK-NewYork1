3e32b264e3753c09f0e36cfc9bc2c393cd3bc594
Gabriel Petz <franciscogpetz@gmail.com>
2022-04-05 19:50:58 -0300

    implementado ReleaseDatePrecision

--- app/src/main/java/ayds/newyork/songinfo/home/view/ReleaseDateMapper.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/view/ReleaseDateMapper.kt
@@ -7,20 +7,23 @@ interface ReleaseDateMapper {
 }
 
 internal class ReleaseDateMapperImpl : ReleaseDateMapper {
-    override fun ReleaseDatePrecision(song: Song): String {
-        return "TODO"
-    }
+    override fun ReleaseDatePrecision(song: Song): String =
+        when (song.releaseDatePrecision) {
+            "day" -> ReleaseDatePrecisionDay()
+            "month" -> ReleaseDatePrecisionMonth()
+            "year" -> ReleaseDatePrecisionYear()
+            else -> "error"
+        }
+}
 
-    private fun ReleaseDatePrecisionDay(): String {
-        return "TODO"
-    }
+private fun ReleaseDatePrecisionDay(): String {
+    return "TODO"
+}
 
-    private fun ReleaseDatePrecisionMonth(): String {
-        return "TODO"
-    }
+private fun ReleaseDatePrecisionMonth(): String {
+    return "TODO"
+}
 
-    private fun ReleaseDatePrecisionYear(): String {
-        return "TODO"
-    }
-
+private fun ReleaseDatePrecisionYear(): String {
+    return "TODO"
 }
