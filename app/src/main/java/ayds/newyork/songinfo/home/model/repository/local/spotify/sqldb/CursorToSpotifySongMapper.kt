d655e4d4a379edb7ac8a0218310f0e8816910f8b
Gabriel Petz <franciscogpetz@gmail.com>
2022-04-13 21:47:59 -0300

    arreglos en la base de datos local

--- app/src/main/java/ayds/newyork/songinfo/home/model/repository/local/spotify/sqldb/CursorToSpotifySongMapper.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/model/repository/local/spotify/sqldb/CursorToSpotifySongMapper.kt
@@ -17,19 +17,15 @@ internal class CursorToSpotifySongMapperImpl : CursorToSpotifySongMapper {
         try {
             with(cursor) {
                 if (moveToNext()) {
+                    val storedPrecisionOrdinal = cursor.getInt(getColumnIndexOrThrow(RELEASE_DATE_PRECISION_COLUMN))
+                    val releaseDatePrecision = DatePrecision.values()[storedPrecisionOrdinal]
                     SpotifySong(
                         id = getString(getColumnIndexOrThrow(ID_COLUMN)),
                         songName = getString(getColumnIndexOrThrow(NAME_COLUMN)),
                         artistName = getString(getColumnIndexOrThrow(ARTIST_COLUMN)),
                         albumName = getString(getColumnIndexOrThrow(ALBUM_COLUMN)),
                         releaseDate = getString(getColumnIndexOrThrow(RELEASE_DATE_COLUMN)),
-                        releaseDatePrecision = DatePrecision.valueOf(
-                            getString(
-                                getColumnIndexOrThrow(
-                                    RELEASE_DATE_PRECISION_COLUMN
-                                )
-                            ).uppercase(Locale.getDefault())
-                        ),
+                        releaseDatePrecision = releaseDatePrecision,
                         spotifyUrl = getString(getColumnIndexOrThrow(SPOTIFY_URL_COLUMN)),
                         imageUrl = getString(getColumnIndexOrThrow(IMAGE_URL_COLUMN)),
                     )
