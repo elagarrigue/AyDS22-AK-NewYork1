5962864a16bc2a94812881a75eb2047339439a95
AlbaErik <erik.alba97@gmail.com>
2022-04-17 18:00:23 -0300

    Clean Code y refactor en getArtistInfo

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -6,19 +6,15 @@ import android.os.Bundle
 import ayds.newyork.songinfo.R
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
-import ayds.newyork.songinfo.moredetails.fulllogic.NYTimesAPI
-import ayds.newyork.songinfo.moredetails.fulllogic.DataBase
 import com.google.gson.Gson
 import com.google.gson.JsonObject
-import com.google.gson.JsonElement
-import ayds.newyork.songinfo.moredetails.fulllogic.OtherInfoWindow
 import android.content.Intent
 import android.net.Uri
 import com.squareup.picasso.Picasso
 import android.text.Html
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import com.google.gson.JsonElement
 import retrofit2.Response
 import java.io.IOException
 import java.lang.StringBuilder
@@ -35,68 +31,66 @@ class OtherInfoWindow : AppCompatActivity() {
         open(intent.getStringExtra("artistName"))
     }
 
-    fun getARtistInfo(artistName: String?) {
+    private fun getArtistInfo(artistName: String?) {
 
-        // create
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.nytimes.com/svc/search/v2/")
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
-        val NYTimesAPI = retrofit.create(NYTimesAPI::class.java)
-        Log.e("TAG", "artistName $artistName")
+        val apiNYTimes = retrofit.create(NYTimesAPI::class.java)
         Thread {
-            var text = DataBase.getInfo(dataBase, artistName)
-            if (text != null) { // exists in db
-                text = "[*]$text"
-            } else { // get from service
+            var artistNameDB = DataBase.getInfo(dataBase, artistName)
+            if (artistNameDB != null) {
+                artistNameDB = "[*]$artistNameDB"
+            } else {
                 val callResponse: Response<String>
                 try {
-                    callResponse = NYTimesAPI.getArtistInfo(artistName).execute()
-                    Log.e("TAG", "JSON " + callResponse.body())
+                    callResponse = apiNYTimes.getArtistInfo(artistName).execute()
                     val gson = Gson()
                     val jobj = gson.fromJson(callResponse.body(), JsonObject::class.java)
                     val response = jobj["response"].asJsonObject
-                    val _abstract = response["docs"].asJsonArray[0].asJsonObject["abstract"]
+                    val abstract = response["docs"].asJsonArray[0].asJsonObject["abstract"]
                     val url = response["docs"].asJsonArray[0].asJsonObject["web_url"]
-                    if (_abstract == null) {
-                        text = "No Results"
+                    if (abstract == null) {
+                        artistNameDB = "No Results"
                     } else {
-                        text = _abstract.asString.replace("\\n", "\n")
-                        text = textToHtml(text, artistName)
+                        artistNameDB = abstract.asString.replace("\\n", "\n")
+                        artistNameDB = textToHtml(artistNameDB, artistName)
 
-
-                        // save to DB  <o/
-                        DataBase.saveArtist(dataBase, artistName, text)
+                        DataBase.saveArtist(dataBase, artistName, artistNameDB)
                     }
-                    val urlString = url.asString
-                    findViewById<View>(R.id.openUrlButton).setOnClickListener {
-                        val intent = Intent(Intent.ACTION_VIEW)
-                        intent.data = Uri.parse(urlString)
-                        startActivity(intent)
-                    }
+                    updateUrlButton(url)
                 } catch (e1: IOException) {
-                    Log.e("TAG", "Error $e1")
                     e1.printStackTrace()
                 }
             }
-            val imageUrl =
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
-            Log.e("TAG", "Get Image from $imageUrl")
-            val finalText = text
-            runOnUiThread {
-                Picasso.get().load(imageUrl).into(findViewById<View>(R.id.imageView) as ImageView)
-                textPane2!!.text = Html.fromHtml(finalText)
-            }
+            updateArtistData(artistNameDB)
         }.start()
+    }
+
+    private fun updateUrlButton(url: JsonElement) {
+        val urlString = url.asString
+        findViewById<View>(R.id.openUrlButton).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(urlString)
+            startActivity(intent)
+        }
+    }
+
+    private fun updateArtistData(artistNameDB: String) {
+        val imageUrl =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
+        runOnUiThread {
+            Picasso.get().load(imageUrl).into(findViewById<View>(R.id.imageView) as ImageView)
+            textPane2!!.text = Html.fromHtml(artistNameDB)
+        }
     }
 
     private var dataBase: DataBase? = null
     private fun open(artist: String?) {
         dataBase = DataBase(this)
         DataBase.saveArtist(dataBase, "test", "sarasa")
-        Log.e("TAG", "" + DataBase.getInfo(dataBase, "test"))
-        Log.e("TAG", "" + DataBase.getInfo(dataBase, "nada"))
-        getARtistInfo(artist)
+        getArtistInfo(artist)
     }
 
     companion object {
