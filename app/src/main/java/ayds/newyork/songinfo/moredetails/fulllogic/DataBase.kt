df4e52652d9b09f70b73a470eaa79ef30a14b490
Gabriel Petz <franciscogpetz@gmail.com>
2022-04-17 17:59:28 -0300

    Refactor de la funcion getInfo

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
@@ -2,16 +2,27 @@ package ayds.newyork.songinfo.moredetails.fulllogic
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-private const val ARTISTS_TABLE_NAME="artists"
-private const val ARTIST_NAME_COLUMN="artist"
-private const val INFO_COLUMN="info"
-private const val SOURCE_COLUMN="source"
-private const val SOURCE_VALUE=1
+private const val ARTIST_ID_COLUMN = "id"
+private const val ARTISTS_TABLE_NAME = "artists"
+private const val ARTIST_NAME_COLUMN = "artist"
+private const val INFO_COLUMN = "info"
+private const val SOURCE_COLUMN = "source"
+private const val SOURCE_VALUE = 1
+private const val DATABASE_NAME = "artists.db"
+private const val DATABASE_VERSION = 1
 
-class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", null, 1) {
+class DataBase(context: Context?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    private val projection = arrayOf(
+        ARTIST_ID_COLUMN,
+        ARTIST_NAME_COLUMN,
+        INFO_COLUMN
+    )
 
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(
@@ -23,40 +34,38 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
 
     fun saveArtist(artist: String?, info: String?) {
         val values = ContentValues().apply {
-            put(ARTIST_NAME_COLUMN,artist)
+            put(ARTIST_NAME_COLUMN, artist)
             put(INFO_COLUMN, info)
             put(SOURCE_COLUMN, SOURCE_VALUE)
         }
         writableDatabase?.insert(ARTISTS_TABLE_NAME, null, values)
     }
 
-    fun getInfo(dbHelper: DataBase, artist: String): String? {
-        val db = dbHelper.readableDatabase
-        val projection = arrayOf(
-            "id",
-            "artist",
-            "info"
-        )
-        val selection = "artist  = ?"
-        val selectionArgs = arrayOf(artist)
-        val sortOrder = "artist DESC"
-        val cursor = db.query(
-            "artists",
+    fun getArtistInfo(artistName: String): String? {
+        val cursor = readableDatabase.query(
+            ARTISTS_TABLE_NAME,
             projection,
-            selection,
-            selectionArgs,
+            "$ARTIST_NAME_COLUMN = ?",
+            arrayOf(artistName),
             null,
             null,
-            sortOrder
+            "$ARTIST_NAME_COLUMN DESC"
         )
-        val items: MutableList<String> = ArrayList()
-        while (cursor.moveToNext()) {
-            val info = cursor.getString(
-                cursor.getColumnIndexOrThrow("info")
-            )
-            items.add(info)
+        return mapToList(cursor)?.get(0)
+    }
+
+    private fun mapToList(cursor: Cursor): MutableList<String>? {
+        val informationCollection: MutableList<String> = ArrayList()
+        with(cursor) {
+            if (moveToNext()) {
+                val info = cursor.getString(
+                    cursor.getColumnIndexOrThrow(INFO_COLUMN)
+                )
+                informationCollection.add(info)
+            } else
+                return null
         }
         cursor.close()
-        return if (items.isEmpty()) null else items[0]
+        return informationCollection
     }
 }
