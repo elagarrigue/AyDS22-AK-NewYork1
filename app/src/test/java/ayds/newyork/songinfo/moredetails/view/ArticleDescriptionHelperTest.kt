a8e1654be8183efd1f0bb128ec0c4b6da4ab7408
AlbaErik <erik.alba97@gmail.com>
2022-05-18 19:37:15 -0300

    ArticleDescriptionHelperTest corregido

--- app/src/test/java/ayds/newyork/songinfo/moredetails/view/ArticleDescriptionHelperTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/moredetails/view/ArticleDescriptionHelperTest.kt
@@ -2,9 +2,12 @@ package ayds.newyork.songinfo.moredetails.view
 
 import ayds.newyork.songinfo.moredetails.model.entities.Article
 import ayds.newyork.songinfo.moredetails.model.entities.NYArticle
-import io.mockk.mockk
 import org.junit.Assert
 import org.junit.Test
+
+private const val HTML_DIV_WIDTH = "<html><div width=400>"
+private const val HTML_FONT = "<font face=\"arial\">"
+private const val HTML_END_TAGS = "</font></div></html>"
 
 class ArticleDescriptionHelperTest {
     private val articleDescriptionHelper by lazy { ArticleDescriptionHelperImpl() }
@@ -21,9 +24,12 @@ class ArticleDescriptionHelperTest {
 
         val artistName = "Patricio Rey y sus Redonditos de Ricota"
 
-        val result = articleDescriptionHelper.textToHtml(article, artistName)
+        var result = articleDescriptionHelper.textToHtml(article, artistName)
+        result = result.replace(HTML_DIV_WIDTH, "")
+        result = result.replace(HTML_FONT, "")
+        result = result.replace(HTML_END_TAGS, "")
 
-        val expected = StringBuilder("[*]reactions of kin of freed Amers")
+        val expected = "[*]reactions of kin of freed Amers"
 
         Assert.assertEquals(expected, result)
 
@@ -32,31 +38,25 @@ class ArticleDescriptionHelperTest {
     @Test
     fun `given a non local article it should return the description`() {
 
+        val articleDescription = "Mr Cerati had platinum success in " +
+                "the Spanish rock and pop world and worked " +
+                "with artist like Shakira"
         val article: Article = NYArticle(
-            "reactions of kin of freed Amers",
+            articleDescription,
             "url",
             1,
-            true
+            false
         )
 
-        val artistName = "Patricio Rey y sus Redonditos de Ricota"
+        val artistName = "Gustavo Cerati"
 
-        val result = articleDescriptionHelper.textToHtml(article, artistName)
-
-        val expected = StringBuilder("reactions of kin of freed Amers")
-
-        Assert.assertEquals(expected, result)
+        var result = articleDescriptionHelper.textToHtml(article, artistName)
+        result = result.replace(HTML_DIV_WIDTH, "")
+        result = result.replace(HTML_FONT, "")
+        result = result.replace(HTML_END_TAGS, "")
+        
+        Assert.assertEquals(articleDescription, result)
 
     }
 
-    @Test
-    fun `given a non New York article it should return the empty NYArticle description`() {
-        val article: Article = mockk()
-
-        val result = articleDescriptionHelper.textToHtml(article, "")
-
-        val expected = "Article not found"
-
-        Assert.assertEquals(expected, result)
-    }
 }
