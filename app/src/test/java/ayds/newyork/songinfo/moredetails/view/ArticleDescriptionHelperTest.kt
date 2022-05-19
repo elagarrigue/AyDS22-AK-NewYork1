cd60a8c6b13c9768a736eb64aae1e88048341368
AlbaErik <erik.alba97@gmail.com>
2022-05-19 19:04:13 -0300

    Nuevos tests para HelperTest
    Se controla el formato del HTML devuelto
    Se controla el resultado obtenido al buscar articulos sin descripcion

--- app/src/test/java/ayds/newyork/songinfo/moredetails/view/ArticleDescriptionHelperTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/moredetails/view/ArticleDescriptionHelperTest.kt
@@ -2,12 +2,14 @@ package ayds.newyork.songinfo.moredetails.view
 
 import ayds.newyork.songinfo.moredetails.model.entities.Article
 import ayds.newyork.songinfo.moredetails.model.entities.NYArticle
+import io.mockk.mockk
 import org.junit.Assert
 import org.junit.Test
 
 private const val HTML_DIV_WIDTH = "<html><div width=400>"
 private const val HTML_FONT = "<font face=\"arial\">"
 private const val HTML_END_TAGS = "</font></div></html>"
+private const val SONG_FOUND_LOCAL = "[*]"
 
 class ArticleDescriptionHelperTest {
     private val articleDescriptionHelper by lazy { ArticleDescriptionHelperImpl() }
@@ -54,9 +56,64 @@ class ArticleDescriptionHelperTest {
         result = result.replace(HTML_DIV_WIDTH, "")
         result = result.replace(HTML_FONT, "")
         result = result.replace(HTML_END_TAGS, "")
-        
+
         Assert.assertEquals(articleDescription, result)
 
     }
 
+    @Test
+    fun `given a non local article it should return the description with correct format`() {
+
+        val articleDescription = "Mr Cerati had platinum success in " +
+                "the Spanish rock and pop world and worked " +
+                "with artist like Shakira"
+        val article: Article = NYArticle(
+            articleDescription,
+            "url",
+            1,
+            false
+        )
+
+        val artistName = "Gustavo Cerati"
+        val expected = HTML_DIV_WIDTH + HTML_FONT + articleDescription + HTML_END_TAGS
+
+        val result = articleDescriptionHelper.textToHtml(article, artistName)
+
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a local article it should return the description with correct format`() {
+        val articleDescription = "Mr Cerati had platinum success in " +
+                "the Spanish rock and pop world and worked " +
+                "with artist like Shakira"
+        val article: Article = NYArticle(
+            articleDescription,
+            "url",
+            1,
+            true
+        )
+
+        val artistName = "Gustavo Cerati"
+        val expected =
+            HTML_DIV_WIDTH + HTML_FONT + SONG_FOUND_LOCAL + articleDescription + HTML_END_TAGS
+
+        val result = articleDescriptionHelper.textToHtml(article, artistName)
+
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a non local article it should return the article not found description`() {
+        val article: Article = mockk()
+
+        var result = articleDescriptionHelper.textToHtml(article, "")
+        result = result.replace(HTML_DIV_WIDTH, "")
+        result = result.replace(HTML_FONT, "")
+        result = result.replace(HTML_END_TAGS, "")
+
+        val expected = "article not found"
+
+        Assert.assertEquals(expected, result)
+    }
 }
