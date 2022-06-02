f4deac80bc4c39cf5bd956fcace0babd42425298
AlbaErik <erik.alba97@gmail.com>
2022-06-02 19:36:05 -0300

    Se agrega libreria LastFM, Proxy LastFM implementado

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/Proxy.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/Proxy.kt
@@ -3,5 +3,5 @@ package ayds.newyork.songinfo.moredetails.model.repository.external.broker
 import ayds.newyork.songinfo.moredetails.model.entities.Card
 
 interface Proxy {
-    fun getCard(artistName: String): Card?
+    fun getCard(artistName: String): Card
 }
--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/ProxyLastFM.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/ProxyLastFM.kt
@@ -1,9 +1,31 @@
 package ayds.newyork.songinfo.moredetails.model.repository.external.broker
 
+import ayds.lisboa2.lastFM.LASTFM_LOGO
+import ayds.lisboa2.lastFM.LastFMArtist
+import ayds.lisboa2.lastFM.LastFMService
 import ayds.newyork.songinfo.moredetails.model.entities.Card
+import ayds.newyork.songinfo.moredetails.model.entities.EmptyCard
+import ayds.newyork.songinfo.moredetails.model.entities.FullCard
+import ayds.newyork.songinfo.moredetails.model.entities.InfoSource
 
-class ProxyLastFM(): Proxy {
-    override fun getCard(artistName: String): Card? {
-        TODO("Not yet implemented")
+class ProxyLastFM(
+    private val lastFMService: LastFMService
+) : Proxy {
+
+    override fun getCard(artistName: String): Card {
+        val artistInfo = lastFMService.getArtist(artistName)
+        return getCardFromService(artistInfo)
     }
+    private fun getCardFromService(article: LastFMArtist?): Card =
+        article?.let {
+            FullCard(
+                it.description,
+                it.infoURL,
+                it.artistName,
+                InfoSource.LastFM,
+                LASTFM_LOGO
+            )
+        }.run {
+            EmptyCard
+        }
 }
