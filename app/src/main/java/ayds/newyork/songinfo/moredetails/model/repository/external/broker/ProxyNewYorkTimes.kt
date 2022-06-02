563514e57d40509f19f64e5f1b9c80a0c97c6c14
AlbaErik <erik.alba97@gmail.com>
2022-06-02 18:42:00 -0300

    Proxy NewYorkTimes desarrollado

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/ProxyNewYorkTimes.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/ProxyNewYorkTimes.kt
@@ -1,12 +1,31 @@
 package ayds.newyork.songinfo.moredetails.model.repository.external.broker
 
+import ayds.ak1.newyorktimes.article.external.NYArticle
 import ayds.ak1.newyorktimes.article.external.NYInfoService
 import ayds.newyork.songinfo.moredetails.model.entities.Card
+import ayds.newyork.songinfo.moredetails.model.entities.EmptyCard
+import ayds.newyork.songinfo.moredetails.model.entities.FullCard
+import ayds.newyork.songinfo.moredetails.model.entities.InfoSource
 
 class ProxyNewYorkTimes(
     private val NYTimesService: NYInfoService
-): Proxy {
-    override fun getCard(artistName: String): Card? {
-        TODO("Not yet implemented")
+) : Proxy {
+    override fun getCard(artistName: String): Card {
+        val infoArtist = NYTimesService.getArtistInfo(artistName)
+        return getCardFromService(infoArtist)
     }
+
+    private fun getCardFromService(article: NYArticle?): Card =
+        article?.let {
+            FullCard(
+                it.description,
+                it.infoURL,
+                it.artistName,
+                InfoSource.NewYorkTimes,
+                it.logoURL
+            )
+        }.run {
+            EmptyCard
+        }
+
 }
