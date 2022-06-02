f8a445a7e2966681293444a147dfbadf2ee96628
Gabriel Petz <franciscogpetz@gmail.com>
2022-06-02 20:20:32 -0300

    agregados proxys en el injector de model

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModelInjector.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModelInjector.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import ayds.newyork.songinfo.moredetails.model.repository.CardRepository
 import ayds.newyork.songinfo.moredetails.model.repository.CardRepositoryImpl
 import ayds.ak1.newyorktimes.article.external.NYInjector
-import ayds.newyork.songinfo.moredetails.model.repository.external.broker.Broker
+import ayds.lisboa2.lastFM.LastFMInjector
+import ayds.newyork.songinfo.moredetails.model.repository.external.broker.*
 import ayds.newyork.songinfo.moredetails.model.repository.external.broker.BrokerImpl
-import ayds.newyork.songinfo.moredetails.model.repository.external.broker.Proxy
-import ayds.newyork.songinfo.moredetails.model.repository.external.broker.ProxyNewYorkTimes
 import ayds.newyork.songinfo.moredetails.model.repository.local.CardLocalStorage
 import ayds.newyork.songinfo.moredetails.model.repository.local.sqldb.CardLocalStorageImpl
 import ayds.newyork.songinfo.moredetails.model.repository.local.sqldb.CursorToCardMapperImpl
 import ayds.newyork.songinfo.moredetails.view.MoreDetailsView
+import ayds.winchester1.wikipedia.WikipediaInjector
 
 object MoreDetailsModelInjector {
 
@@ -26,6 +26,8 @@ object MoreDetailsModelInjector {
         )
 
         val proxyNYTimes: Proxy = ProxyNewYorkTimes(NYInjector.nyInfoService)
+        val proxyWikipedia: Proxy = ProxyWikipedia(WikipediaInjector.wikipediaCardService)
+        val proxyLastFM: Proxy = ProxyLastFM(LastFMInjector.lastFMService)
         val cardBroker: Broker = BrokerImpl(listOf(proxyLastFM, proxyWikipedia, proxyNYTimes))
 
         val repository: CardRepository =
