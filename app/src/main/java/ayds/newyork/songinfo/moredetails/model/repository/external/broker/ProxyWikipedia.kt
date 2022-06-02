ffd03c3d85da9602a092ce17a779ed077d29b67f
AlbaErik <erik.alba97@gmail.com>
2022-06-02 19:55:00 -0300

    Proxy Wikipedia implementando

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/ProxyWikipedia.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/broker/ProxyWikipedia.kt
@@ -1,9 +1,31 @@
 package ayds.newyork.songinfo.moredetails.model.repository.external.broker
 
+import ayds.lisboa2.lastFM.LASTFM_LOGO
 import ayds.newyork.songinfo.moredetails.model.entities.Card
+import ayds.newyork.songinfo.moredetails.model.entities.EmptyCard
+import ayds.newyork.songinfo.moredetails.model.entities.FullCard
+import ayds.newyork.songinfo.moredetails.model.entities.InfoSource
+import ayds.winchester1.wikipedia.WikipediaArtistInfo
+import ayds.winchester1.wikipedia.WikipediaCardService
 
-class ProxyWikipedia(): Proxy {
-    override fun getCard(artistName: String): Card? {
-        TODO("Not yet implemented")
+class ProxyWikipedia(
+    private val wikiService: WikipediaCardService
+): Proxy {
+    override fun getCard(artistName: String): Card {
+        val artistInfo = wikiService.getCard(artistName)
+        return getCardFromService(artistInfo)
     }
+
+    private fun getCardFromService(article: WikipediaArtistInfo?): Card =
+        article?.let {
+            FullCard(
+                it.description,
+                it.infoURL,
+                "it.artistName", // CAMBIAR
+                InfoSource.Wikipedia,
+                LASTFM_LOGO // CAMBIAR LINK DEL LOGO
+            )
+        }.run {
+            EmptyCard
+        }
 }
