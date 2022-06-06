ec3fab19ac315cd401ce1c54308cbb196052d8fe
lpakusch <lars.pakusch@st.ovgu.de>
2022-06-06 09:40:53 -0300

    refactoring de funcion existFullCards

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/CardRepository.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/CardRepository.kt
@@ -16,33 +16,34 @@ internal class CardRepositoryImpl(
 
     override fun getCardsByArtistName(artistName: String): List<Card> {
         val repositoryCards = cardLocalStorage.getCards(artistName)
-        var existFullCards=false
+        val existFullCards = checkListForFullCards(repositoryCards)
 
+        return when {
+            existFullCards -> repositoryCards
+            else -> {
+                val brokerCardList = broker.getCards(artistName)
+                    for(Card in brokerCardList) {
+                        if (Card!=EmptyCard) {
+                            cardLocalStorage.saveCard(artistName,Card)
+                        }
+                    }
+                brokerCardList
+            }
+        }
+    }
+
+    private fun checkListForFullCards(repositoryCards:List<Card>):Boolean {
+        var existFullCards=false
         for (Card in repositoryCards){
             if (Card!=EmptyCard){
                 markCardAsLocal(Card)
                 existFullCards=true
             }
-
         }
-
-        when {
-            existFullCards -> return repositoryCards
-
-            else -> {
-                val brokerCardList = broker.getCards(artistName)
-                if (brokerCardList.isNotEmpty())
-                    for(Card in brokerCardList) {
-                        cardLocalStorage.saveCard(artistName,Card)
-                    }
-                return brokerCardList
-            }
-        }
+        return existFullCards
     }
 
-    private fun markCardAsLocal(card: Card?) {
-        if (card != null) {
-            card.isLocallyStored = true
-        }
+    private fun markCardAsLocal(card: Card) {
+        card.isLocallyStored = true
     }
 }
