8ecc680c4b1b17fcd37205c9ddd7418d9f70c445
lpakusch <lars.pakusch@st.ovgu.de>
2022-06-07 18:33:48 -0300

    Fix bug outOfIndex

--- app/src/main/java/ayds/newyork/songinfo/moredetails/view/MoreDetailsUiState.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/view/MoreDetailsUiState.kt
@@ -1,13 +1,20 @@
 package ayds.newyork.songinfo.moredetails.view
 
 import ayds.newyork.songinfo.moredetails.model.entities.Card
+import ayds.newyork.songinfo.moredetails.model.entities.EmptyCard
 
 data class MoreDetailsUiState(
     val cardList: List<Card> = listOf(),
     val artistName: String = "",
     val positionSpinner: Int = 0,
 ) {
-    fun getCurrentCard(): Card = cardList[positionSpinner]
+    fun getCurrentCard(): Card {
+        return if (positionSpinner>(cardList.size-1))
+            EmptyCard
+        else
+            cardList[positionSpinner]
+    }
+
 
     companion object {
         const val LOGO_NOT_FOUND_URL =
